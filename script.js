e84c5b9e5c9d38556b612f2a46d6a151aa00f83e
Sabrina Kennedy <skenne21@gmail.com>
2017-12-06 15:58:26 -0700

    Final game

--- script.js
+++ script.js
@@ -2,35 +2,44 @@ var userGuess = document.querySelector('#user-guess');
 var submitButton = document.querySelector('.submit-btn');
 var clearButton = document.querySelector('.clear-btn');
 var resetButton = document.querySelector('.reset-btn');
+var submitMinMaxButton = document.querySelector('.submit-min-max');
+var clearMinMaxInputButton = document.querySelector('.clear-min-max-values');
+var feedbackText = document.querySelector('.feedback-text');
 var randomNumber;
-var minNumber;
-var maxNumber;
+var childElement;
+var minNumber = 0;
+var maxNumber = 100;
 var counter = 0;
 
 generateRandomNumber();
 
 function userInput() {
   event.preventDefault();
-  counter++;
   userGuess = parseInt(userGuess.value);
+  runGame();
   console.log('userGuess' , userGuess);
-  console.log('gamecounter', counter)
+};
+
+function runGame() {
+  counter++;
   valueIsANumber();
   readySecondaryButtons();
+  createMinAndMaxDisplay();
+  console.log('gamecounter', counter)
 };
 
 function generateRandomNumber() {
-  minNumber = 0;
-  maxNumber = 100;
   randomNumber = Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
   console.log('randomNumber', randomNumber);
+  return randomNumber;
 };
 
 function valueIsANumber() {
   if(isNaN(userGuess) === true) {
-    gameTextHint();
-    document.querySelector('.min-max-text').innerHTML = `
-    Your Was Not In Range, Pick a Number Between ${minNumber}  and ${maxNumber} !`
+    document.querySelector('.ui-text').innerHTML = 'Your Guess Was';
+    document.querySelector('.changed-text').innerHTML = 'Oops!';
+    feedbackText.innerHTML = `
+    Not A Number, Pick a Number Between ${minNumber}  and ${maxNumber} !`;
   } else {
     verifyNumberRange();
   };
@@ -39,8 +48,8 @@ function valueIsANumber() {
 function verifyNumberRange() {
   if (userGuess < minNumber || userGuess > maxNumber) {
     gameTextHint();
-    document.querySelector('.min-max-text').innerHTML = `
-    Your Was Not In Range, Pick a Number Between ${minNumber}  and ${maxNumber} !`;
+    feedbackText.innerHTML = `
+    Not In Range, Pick a Number Between ${minNumber}  and ${maxNumber} !`;
   } else {
     compareNumbers();
   };
@@ -49,10 +58,10 @@ function verifyNumberRange() {
 function compareNumbers() {
   if (userGuess < randomNumber) {
     gameTextHint();
-    document.querySelector('.min-max-text').innerHTML = 'Too Low, Guess Again!';
+    feedbackText.innerHTML = 'Too Low, Guess Again!';
   } else if (userGuess > randomNumber) {
     gameTextHint();
-    document.querySelector('.min-max-text').innerHTML = 'Too High, Guess Again!';
+    feedbackText.innerHTML = 'Too High, Guess Again!';
   } else if (userGuess === randomNumber) {
     correctGuess();
   }; 
@@ -60,11 +69,12 @@ function compareNumbers() {
 
 function correctGuess() {
   gameTextHint();
-  document.querySelector('.min-max-text').innerHTML = 'Boom, You Got It Right, Play Again!';
+  feedbackText.innerHTML = 'Boom, You Got It Right, Play Again!';
   generateRandomNumber();
   minNumber -= 10;
   maxNumber +=10
   counter = 0;
+  submitMinMaxButton.disabled = false;
   console.log('counter', counter, 'min', minNumber, 'max', maxNumber)
 };
 
@@ -76,40 +86,68 @@ function gameTextHint() {
 function readySecondaryButtons() {
   clearButton.addEventListener('click', clearInput);
   resetButton.addEventListener('click', resetGame);
+  clearMinMaxInputButton.addEventListener('click', resetMinMaxInput)
+};
 
-  // reset buttons
+
+function createMinAndMaxDisplay() {
+  var parentElement = document.querySelector('.min-max-section');
+  childElement = document.createElement('div');
+  childElement.setAttribute('class', 'min-max-display');
+  console.log(minNumber, maxNumber)
+  childElement.innerHTML = (`
+    <h4> Min Vaule: ${minNumber} & Max Value: ${maxNumber}</h4>
+    <h4> Number of Guess: ${counter}</h4 >
+    `);
+  parentElement.appendChild(childElement);
+}
+
+function removeMinMaxDisplay() {
+  var parentElement = document.querySelector('.min-max-section');
+  console.log(parentElement)
+  parentElement.removeChild(childElement);
 }
 
 function enableButtons() {
   submitButton.disabled = false; 
   clearButton.disabled = false;
-  if (counter >= 5) {
+  if (counter === 5) {
     resetButton.disabled = false;
-  }
-}
+  };
+};
 
 function clearInput() {
+  console.log(childElement)
   event.preventDefault();
   document.querySelector('#user-guess').value = '';
   userGuess = document.querySelector('#user-guess');
+  removeMinMaxDisplay();
+  disableButtons();
+};
+
+function disableButtons() {
   submitButton.setAttribute('disabled', true);
   clearButton.setAttribute('disabled', true);
-}
+};
 
 function resetGame() {
-  counter = 0;
-  minNumber = 0; 
-  maxNumber = 100;
-  document.querySelector('#user-guess').value = '';
-  document.querySelector('.ui-text').innerHTML = 'Guess a Number';
-  document.querySelector('.changed-text').innerHTML = '';
-  document.querySelector('.min-max-text').innerHTML = 'Between ' + minNumber + ' and ' + maxNumber;
+  window.location.reload(true)
+};
+
+function setMinMaxValues() {
+  event.preventDefault();
+  minNumber = parseInt(document.getElementById('min-value').value);
+  maxNumber= parseInt(document.getElementById('max-value').value);
   generateRandomNumber();
-}
+  resetMinMaxInput();
+  console.log(minNumber, maxNumber) 
+};
 
-submitButton.addEventListener('click', userInput);
+function resetMinMaxInput() {
+  clearMinMaxInputButton.disabled = false;
+  submitMinMaxButton.setAttribute('disabled', true);
+};
+
 userGuess.addEventListener('keyup', enableButtons);
-
-// create ui that changes as the min and max number change
-// create button for user setting own range
-// how do I make random number change as the user wins have min and max 0 to 100 || what user sets or winning value 
+submitButton.addEventListener('click', userInput);
+submitMinMaxButton.addEventListener('click', setMinMaxValues);
